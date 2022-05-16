ffbc4a81ab8de22729d9abcc490966de9feaac98
Bran Liang <lby89757@hotmail.com>
2022-05-16 10:25:12 +0800

    Add fill asembly code

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,70 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@color
+M=0
+// Listen to keyboard input
+(LOOP)
+@KBD
+D=M
+// set the screen color to black if input found
+@SETBLACK
+D;JGT
+// set the screen color to white if otherwise
+@SETWHITE
+D;JEQ
+
+(SETBLACK)
+@color
+M=-1
+@SYNC
+0;JMP
+
+(SETWHITE)
+@color
+M=0
+@SYNC
+0;JMP
+
+(SYNC)
+@row
+M=0
+@SCREEN
+D=A
+@address
+M=D
+@REFRESHROWS
+0;JMP
+
+(REFRESHROWS)
+@col
+M=0
+@row
+D=M
+@256
+D=D-A
+@LOOP
+D;JEQ
+@row
+M=M+1
+@REFRESHROW
+0;JMP
+
+(REFRESHROW)
+@col
+D=M
+@32
+D=D-A
+@REFRESHROWS
+D;JEQ
+@color
+D=M
+@address
+A=M
+M=D
+@address
+M=M+1
+@col
+M=M+1
+@REFRESHROW
+0;JMP
