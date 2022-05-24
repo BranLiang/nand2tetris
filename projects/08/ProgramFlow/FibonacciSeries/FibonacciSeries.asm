0429e5d6fdcdf1ce2c3c9b50175d5adce09b3fc3
Bran Liang <lby89757@hotmail.com>
2022-05-24 12:18:26 +0800

    Pass Fibonacci test

---
+++ projects/08/ProgramFlow/FibonacciSeries/FibonacciSeries.asm
@@ -0,0 +1,263 @@
+// push argument 1
+@ARG
+D=M
+@1
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop pointer 1
+@SP
+AM=M-1
+D=M
+@THAT
+M=D
+// push constant 0
+@0
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop that 0
+@THAT
+D=M
+@0
+D=D+A
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+// push constant 1
+@1
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop that 1
+@THAT
+D=M
+@1
+D=D+A
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+// push argument 0
+@ARG
+D=M
+@0
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 2
+@2
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+@SP
+A=M-1
+D=M
+A=A-1
+D=M-D
+@SP
+A=M-1
+A=A-1
+M=D
+@SP
+M=M-1
+// pop argument 0
+@ARG
+D=M
+@0
+D=D+A
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+// label main_loop_start
+(MAIN_LOOP_START)
+// push argument 0
+@ARG
+D=M
+@0
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// if-goto compute_element
+@SP
+A=M-1
+D=M
+@SP
+M=M-1
+@COMPUTE_ELEMENT
+D;JNE
+// goto end_program
+@END_PROGRAM
+0;JMP
+// label compute_element
+(COMPUTE_ELEMENT)
+// push that 0
+@THAT
+D=M
+@0
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push that 1
+@THAT
+D=M
+@1
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+@SP
+A=M-1
+D=M
+A=A-1
+D=M+D
+@SP
+A=M-1
+A=A-1
+M=D
+@SP
+M=M-1
+// pop that 2
+@THAT
+D=M
+@2
+D=D+A
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+// push pointer 1
+@THAT
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 1
+@1
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+@SP
+A=M-1
+D=M
+A=A-1
+D=M+D
+@SP
+A=M-1
+A=A-1
+M=D
+@SP
+M=M-1
+// pop pointer 1
+@SP
+AM=M-1
+D=M
+@THAT
+M=D
+// push argument 0
+@ARG
+D=M
+@0
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 1
+@1
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+@SP
+A=M-1
+D=M
+A=A-1
+D=M-D
+@SP
+A=M-1
+A=A-1
+M=D
+@SP
+M=M-1
+// pop argument 0
+@ARG
+D=M
+@0
+D=D+A
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+// goto main_loop_start
+@MAIN_LOOP_START
+0;JMP
+// label end_program
+(END_PROGRAM)
+// Program end
+(END)
+@END
+0;JMP
